2ec32074073b2ce221070fb75f5218e064e99d96
Grzegorz <tin91GW@gmail.com>
2023-04-27 18:51:18 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Header.css'
+
+
+export default function Header() {
+
+
+    return (
+        <header className='header'>
+
+            <div className='header_wrapper'>
+                <img src=""></img>
+            </div>
+
+        </header>
+    )
+
+
+
+}
